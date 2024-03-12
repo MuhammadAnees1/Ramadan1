dff86c0c21a7a249b935752b41faf81cd221be1e
office <officeworkemail063@gmail.com>
2024-03-12 07:57:38 +0500

    Initial commit

---
+++ app/src/main/java/com/example/ramadan1/AlarmReceiver.java
@@ -0,0 +1,29 @@
+package com.example.ramadan1;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Alarm alarm = new Alarm(context);
+        alarm.fromIntent(intent);
+
+        if (NotificationActivity.areNotificationsEnabled(context)) {
+            // Notifications are enabled, show the alarm notification
+            NotificationHandler.showAlarmNotification(context);
+        } else {
+            // Notifications are disabled, you may choose to handle it differently
+            Log.i("AlarmReceiver", "Notifications are disabled");
+        }
+        // Start the Notification activity
+        Intent newIntent = new Intent(context, Notification.class);
+        alarm.toIntent(newIntent);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(newIntent);
+    }
+}
